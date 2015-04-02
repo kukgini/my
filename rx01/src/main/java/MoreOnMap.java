8a4752bdf2a9028f638d6508f2b54d4bad3bb644
kukgini <kukgini@gmail.com>
2015-04-02 10:21:23 +0900

    reformat code

--- rx01/src/main/java/MoreOnMap.java
+++ rx01/src/main/java/MoreOnMap.java
@@ -8,17 +8,17 @@ import rx.functions.Func1;
 public class MoreOnMap {
     public static void main(String[] args) {
         Observable.just("Hello. world!")
-                .map(new Func1<String, Integer>() {
-                    @Override
-                    public Integer call(String s) {
-                        return s.hashCode();
-                    }
-                })
-                .subscribe(new Action1<Integer>() {
-                    @Override
-                    public void call(Integer i) {
-                        System.out.println(Integer.toString(i));
-                    }
-                });
+            .map(new Func1<String, Integer>() {
+                @Override
+                public Integer call(String s) {
+                    return s.hashCode();
+                }
+            })
+            .subscribe(new Action1<Integer>() {
+                @Override
+                public void call(Integer i) {
+                    System.out.println(Integer.toString(i));
+                }
+            });
     }
 }
