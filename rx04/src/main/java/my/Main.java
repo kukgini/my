cd8aa3abaaf802a8615dee984a7236ca9180a802
kukgini <kukgini@gmail.com>
2018-02-24 07:07:44 +0000

    single privider multiple consumers

--- rx04/src/main/java/my/Main.java
+++ rx04/src/main/java/my/Main.java
@@ -1,32 +1,46 @@
 package my;
 
 import java.util.*;
+import java.util.function.*;
 import java.util.stream.*;
 import java.util.concurrent.*;
 
 public class Main {
     
-    private static final InfiniteStream<Integer> stream = new InfiniteStream<>();
+    private static final LinkedBlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+    private static Supplier<Stream<Integer>> supplier = () -> Stream.generate(queue::poll);
     
     public static void main(String[] args) {
-        
         System.err.println("now i will make a provider.");
         new Thread(Main::provide).start();
         
-        System.err.println("now i will make a consumer.");
-        stream
-            .filter(x -> x != null)
-            .forEach(System.out::println);
+        System.err.println("now i will make multiple consumers.");
+        int numOfConsumerThreads = 2;
+        ExecutorService executors = Executors.newFixedThreadPool(numOfConsumerThreads);
+    	for (int i = 0; i < numOfConsumerThreads; i++) {
+    		executors.execute(consume(i));
+    	}
     }
     
     private static void provide() {
         for (int i = 0; i < 10; i++) {
             try {
-                stream.accept(i);
+                queue.offer(i);
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 
             }
         }        
+    }
+    
+    private static Runnable consume(final int i) {
+        return new Runnable() {
+            public void run() {
+                supplier.get()
+                    .filter(x -> x != null)
+                    .map(x -> String.format("consumer %s = %d",i, x))
+                    .forEach(System.out::println);
+            }
+        };
     }
 }
