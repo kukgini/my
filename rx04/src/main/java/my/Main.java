17ef243794eb8fd8be3ef94d4522a17863277c89
kukgini <kukgini@gmail.com>
2018-02-24 06:11:19 +0000

    ...

--- rx04/src/main/java/my/Main.java
+++ rx04/src/main/java/my/Main.java
@@ -6,23 +6,23 @@ import java.util.concurrent.*;
 
 public class Main {
     
-    private static final BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+    private static final InfiniteStream<Integer> stream = new InfiniteStream<>();
     
     public static void main(String[] args) {
         
         System.err.println("now i will make a provider.");
-        new Thread(Main::enqueueSomething).start();
+        new Thread(Main::provide).start();
         
         System.err.println("now i will make a consumer.");
-        Stream.generate(() -> queue.poll())
+        stream
             .filter(x -> x != null)
             .forEach(System.out::println);
     }
     
-    private static void enqueueSomething() {
+    private static void provide() {
         for (int i = 0; i < 10; i++) {
             try {
-                queue.put(i);
+                stream.accept(i);
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 
